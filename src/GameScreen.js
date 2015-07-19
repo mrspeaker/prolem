fa6401e91ced17809a8475e6293c94ac53d38249
Mr Speaker <mrspeaker@gmail.com>
2015-07-19 09:03:59 -0400

    get by tag

--- src/GameScreen.js
+++ src/GameScreen.js
@@ -10,7 +10,7 @@ const scene = new Container();
 const renderer = new CanvasRenderer(400, 200);
 document.querySelector('#gameArea').appendChild(renderer.view);
 
-// game.addProgram("red move(10, 10) build(2, 2)")
+// game.addProgram("red move(10, 10) build(2, 2) take(1) thing()")
 
 class GameScreen {
   constructor () {
@@ -18,8 +18,10 @@ class GameScreen {
     this.selection = [];
 
     const p1 = new Perple(20, 20);
+    p1.tag = 'perp';
     scene.add(p1);
     const p2 = new Perple(10, 10);
+    p2.tag = 'perp';
     scene.add(p2);
 
     this.entities.push(p1, p2);
@@ -33,12 +35,12 @@ class GameScreen {
     const first = ast[0];
     if (first.type === 'identifier') {
       // New selection
-      this.selection = [...this.entities];
+      this.selection = this.entities.filter(e => {
+        return e.tag.match(first.value);
+      });
       ast.shift();
     }
 
-    // TODO: get first instruction, if ID, select...
-    // else use current selection.
     this.selection.forEach(s => {
       s.initProgram({
         running: true,
